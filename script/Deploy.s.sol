e94713b029f5a4e40d69614b3589112608da05bd
gretzke <daniel@gretzke.de>
2024-10-17 19:09:36 +0200

    deploy contracts to unichain-sepolia

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,20 +4,24 @@ pragma solidity 0.8.26;
 import {FeeSplitter} from '../src/FeeSplitter/FeeSplitter.sol';
 import {L1Splitter} from '../src/FeeSplitter/L1Splitter.sol';
 import {INetFeeSplitter, NetFeeSplitter} from '../src/FeeSplitter/NetFeeSplitter.sol';
-import {Script} from 'forge-std/Script.sol';
+import {Script, stdJson} from 'forge-std/Script.sol';
 
 contract Deploy is Script {
+    using stdJson for string;
+
     function run() public {
         vm.startBroadcast();
-        address l1Wallet = address(0); // todo
-        uint256 feeDisbursementInterval = 1 days;
-        uint256 withdrawalMinAmount = 0.1 ether;
-        address netFeeRecipient = address(0); // todo
+        string memory input = vm.readFile('script/input.json');
+        string memory chainIdSlug = string(abi.encodePacked('["', vm.toString(block.chainid), '"]'));
+        address l1Wallet = input.readAddress(string.concat(chainIdSlug, '.l1Wallet'));
+        address netFeeRecipient = input.readAddress(string.concat(chainIdSlug, '.netFeeRecipient'));
+        address opWallet = input.readAddress(string.concat(chainIdSlug, '.opWallet'));
+        uint256 feeDisbursementInterval = 1 hours;
+        uint256 withdrawalMinAmount = 0.01 ether;
         INetFeeSplitter.Recipient[] memory recipientData = new INetFeeSplitter.Recipient[](1);
         recipientData[0] = INetFeeSplitter.Recipient({admin: netFeeRecipient, allocation: 10_000});
         address[] memory initialRecipients = new address[](1);
         initialRecipients[0] = netFeeRecipient;
-        address opWallet = address(0); // todo
         L1Splitter l1Splitter = new L1Splitter(l1Wallet, feeDisbursementInterval, withdrawalMinAmount);
         NetFeeSplitter netFeeSplitter = new NetFeeSplitter(initialRecipients, recipientData);
         new FeeSplitter(opWallet, address(l1Splitter), address(netFeeSplitter));

776cc75422a9f236d0c4bac52e78a77710a6d663
Joshua Gutow <joshua.gutow@uniswap.org>
2024-10-30 10:01:47 -0700

    Set OP, UF, & UL addresses

--- script/genesis/GenesisAllocGenerator.s.sol
+++ script/genesis/GenesisAllocGenerator.s.sol
@@ -23,9 +23,9 @@ contract GenesisAllocGenerator is Script {
 
         {
             // deploy optimsism fee forwarder contract and copy code and storage to address
-            address opRecipient = address(1); // Op fee recipient on L1
-            address opAdmin = address(2); // Admin of fee forwarder on L1
-            uint48 opFeeDisbursementInterval = 7 days; // TODO set correct value
+            address opRecipient = address(0xa3d596EAfaB6B13Ab18D40FaE1A962700C84ADEa); // Op fee recipient on L1
+            address opAdmin = address(0xb4e696eafab6b13ab18d40fae1a962700c84befb); // Admin of fee forwarder on L1
+            uint48 opFeeDisbursementInterval = 1 days; // TODO set correct value
             uint256 opMinWithdrawalAmount = 10 ether; // TODO set correct value
             address opForwarder =
                 address(new L1Splitter(opAdmin, opRecipient, opFeeDisbursementInterval, opMinWithdrawalAmount));
@@ -36,8 +36,8 @@ contract GenesisAllocGenerator is Script {
 
         {
             // deploy l1 fee forwarder contract and copy code and storage to address
-            address l1Wallet = address(3); // L1 fee recipient on L1
-            address l1Admin = address(4); // Admin of fee forwarder on L1
+            address l1Wallet = address(0x2F60A5184c63ca94f82a27100643DbAbe4F3f7Fd); // L1 fee recipient on L1
+            address l1Admin = address(0xa356d5d10aa8a842b31530de71ea86c0760cb2c2); // Admin of fee forwarder on L1
             uint48 l1FeeDisbursementInterval = 1 days; // TODO set correct value
             uint256 l1MinWithdrawalAmount = 1 ether; // TODO set correct value
             address l1Forwarder =
@@ -49,9 +49,9 @@ contract GenesisAllocGenerator is Script {
 
         {
             // deploy net fee splitter contract and copy code and storage to address
-            address labsRecipient = address(5); // temporary EOA admin + recipient for labs
+            address labsRecipient = address(0xaE85BbB6c1c1807a64A88F1a1f978740c8a0Dba0); // temporary EOA admin + recipient for labs
             uint256 labsAllocation = 2500; // TODO set correct value
-            address foundationRecipient = address(6); // temporary EOA admin + recipient for foundation
+            address foundationRecipient = address(0x3fCbACd76037534D2AAeB9a17f4e631dd64fbe31); // temporary EOA admin + recipient for foundation
             uint256 foundationAllocation = 7500; // TODO set correct value
 
             INetFeeSplitter.Recipient[] memory recipientData = new INetFeeSplitter.Recipient[](2);

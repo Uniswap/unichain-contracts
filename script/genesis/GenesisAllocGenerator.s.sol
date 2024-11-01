11b3c313dc2cf0d4960ece45e020385b75495d64
gretzke <daniel@gretzke.de>
2024-11-01 17:26:25 +0100

    set l1 fee parameters

--- script/genesis/GenesisAllocGenerator.s.sol
+++ script/genesis/GenesisAllocGenerator.s.sol
@@ -38,8 +38,8 @@ contract GenesisAllocGenerator is Script {
             // deploy l1 fee forwarder contract and copy code and storage to address
             address l1Wallet = address(0x2F60A5184c63ca94f82a27100643DbAbe4F3f7Fd); // L1 fee recipient on L1
             address l1Admin = address(0xa356d5D10aA8A842B31530dE71EA86c0760CB2C2); // Admin of fee forwarder on L1
-            uint48 l1FeeDisbursementInterval = 1 days; // TODO set correct value
-            uint256 l1MinWithdrawalAmount = 1 ether; // TODO set correct value
+            uint48 l1FeeDisbursementInterval = 1 days;
+            uint256 l1MinWithdrawalAmount = 10 ether;
             address l1Forwarder =
                 address(new L1Splitter(l1Admin, l1Wallet, l1FeeDisbursementInterval, l1MinWithdrawalAmount));
             vm.etch(l1ForwarderAddress, l1Forwarder.code);

432941c25c797829c8825709b694f3cd4b328c3c
gretzke <daniel@gretzke.de>
2025-03-17 23:13:56 +0100

    Add explanation for added function to interface

--- src/UVN/L1/StakingMiddleware.sol
+++ src/UVN/L1/StakingMiddleware.sol
@@ -17,6 +17,7 @@ contract StakingMiddleware is SlashingManager, IStakingMiddleware {
     {}
 
     /// @inheritdoc Nonces
+    /// @dev The `Nonces` library does not come with an interface, to ensure that the function is included in the interface of this contract, add it here
     function nonces(address owner) public view override(Nonces, IStakingMiddleware) returns (uint256) {
         return super.nonces(owner);
     }

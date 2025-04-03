f4f6fc98dc8e860de5079315a49333357be656bd
gretzke <daniel@gretzke.de>
2025-04-03 02:56:58 +0200

    Add OptimismPortal2

---
+++ test/helpers/OptimismPortal2Helper.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import OptimismPortal2 to add it to the compilation artifacts
+import {OptimismPortal2} from '@eth-optimism-bedrock/src/L1/OptimismPortal2.sol';
